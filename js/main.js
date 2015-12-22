0eae2053b4bb037d239852cc740e2a52e5f2c5c4
Jonathan Koser <jonathan.koser@gmail.com>
2015-12-21 23:28:31 -0600

    Some things
    Did some stuff

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
     
-        
+    var cities; //collection of city layers
+    
+    //set the sizes and keep them up to date
+    var h; //create the variable for height
+    resize();
+    window.onresize = resize();
+    
+    
     //set up a basic map window by selecting the "map" div element in index.html
 	var map = L.map("map", {
 		center: [44.5, 13.6],
@@ -8,6 +15,7 @@ $(document).ready(function() {
 		maxZoom: 10,
 		minZoom: 3
 	});
+    
     
     
     //identify which tile layer to use and then add it to the map
@@ -19,28 +27,41 @@ $(document).ready(function() {
     
     //use jquery to get the GeoJSON data and send it to be processed
 	//loads the geojson then passes it to the .done method to be used in the 
-	//anonymous function as "data". Need to do this for L.geoJson to work (needs a feature)
-	//if it failed, it'll excecute the alert
+	//anonymous function as "data". Need to do this for L.geoJson to work
+	//(needs it parsed into features) if it failed, it'll excecute the alert
 	$.getJSON("data/citrip.geojson").done(function(data) {
+        //create the city markers
         markCities(data);
 
         
 	}).fail(function() {alert ("There has been a problem loading the data.")}); 
     
+    
+    
+    
+    
+    
+    //=============FUNCTIONS=====================//
+    
     //adds the cities markers to the map
     function markCities(data) {
-      cities = L.geoJson(data, {
-          pointToLayer: function(feature, latln) {
-              return L.circleMarker(latln);
-          }
+        //create the cities layers
+        cities = L.geoJson(data, {
+            pointToLayer: function(feature, latln) {
+                return L.circleMarker(latln)
+            }
           
-      }).addTo(map);  
+        }).addTo(map);  
         
     }; //end markCities function
     
     
-    
-    
+    //resizes the elements based off of screen height
+    function resize() {
+        h = window.innerHeight
+        $("#map").css("height", h);
+        $("#sidebar").css("height", h);
+    }
     
     
 }) //end document ready function
