815df23ce83b939b1f4145ab9a25ac209a9fa29e
Jonathan Koser <jonathan.koser@gmail.com>
2016-01-09 18:41:59 -0600

    Icons change now
    Now, the icons change color when clicked. Also began writing
    infrastructure so that when a user  navigates with the arrows the
    corresponding city will be selected.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
     
     var cities; //collection of leaflet layers - one for each city
     var citiesArray = []; //collection of city layers
-    var currentCity = 1; //the order number of the currently selected city
+    var currentCity; //the order number of the currently selected city
+    var currentMarker; //the marker icon which is currently selected
     
     //set the sizes and keep them up to date
     var h; //create the variable for height
@@ -14,11 +15,25 @@ $(document).ready(function() {
 	var map = L.map("map", {
 		center: [44.5, 13.6],
 		zoom: 7,
-		maxZoom: 10,
 		minZoom: 3
 	});
     
+    //createes the "selected" icon
+    var selectedIcon = L.icon({
+        iconUrl: 'js/images/marker-icon-red.png',
+        iconRetinaUrl: 'js/images/marker-icon-2x-red.png',
+        shadowUrl: 'js/images/marker-shadow.png',
+        popupAnchor: [0,-40],
+        iconAnchor: [13,40]
+    });
     
+    var nonSelectedIcon = L.icon ({
+        iconUrl: 'js/images/marker-icon.png',
+        iconRetinaUrl: 'js/images/marker-icon-2x.png',
+        shadowUrl: 'js/images/marker-shadow.png',
+        popupAnchor: [0,-40],
+        iconAnchor: [13,40]
+    })
     
     //identify which tile layer to use and then add it to the map
 	L.tileLayer(
@@ -32,14 +47,14 @@ $(document).ready(function() {
 	//anonymous function to complete the process. Need to do this for L.geoJson to work
 	//(needs it processed into a collection of geoJSON features) if it failed, it'll excecute the alert    
 	$.getJSON("data/citrip.geojson").done(function(data) {
+        //for first load, set current city to Rovinj
+        currentCity = 1;
         //create the city markers
         markCities(data);
-        console.log(data)
         //create an array of city features from the processed data - easier to access properties
         citiesArray = data.features;
         //set up the navigation
         navigate();
-
         
 	}).fail(function() {alert ("There has been a problem loading the data.")}); 
 
@@ -97,19 +112,44 @@ $(document).ready(function() {
         
     }; //end markCities function
     
-    
-    //creates the marker icon
-    function makeMarker(feature, latlng) {
-        return L.marker(latlng)
+    //creates the markers
+    function makeMarker(feature, latlng) {   
+        //gives Rovinj a red marker to begin with
+        var useIcon;
+        if (feature.properties.Order == 1) {
+            useIcon = selectedIcon;
+        }
+        else {
+            useIcon = nonSelectedIcon;
+        }
+        //creates the new marker
+        var newMarker = L.marker(latlng, {icon: useIcon, markerID: feature.properties.Order}).on('click', function (e) {
+            //makes sure that current marker has a value before it sets the icon on it
+            if (currentMarker != null) {
+                //sets the old red marker back to the blue one
+                currentMarker.setIcon(nonSelectedIcon)
+            };
+            //sets the clicked icon (e.target) to the red one
+            e.target.setIcon(selectedIcon);
+            //assigns the currently selected marker as the one that was clicked
+            currentMarker = e.target;
+
+        })
+        
+        //sets the current marker with the one associated with the first city when currentMarker doesn't yet have a value
+        if (currentMarker == null && feature.properties.Order == 1) {
+            //assigns the current marker to the marker corresponding to the first city in the order (Rovinj)
+            currentMarker = newMarker;
+        };
+        
+        return newMarker;
     }; //end makeMarker
     
     
     //for each feature
     function onEachCity(feature, layer) {
         //binds the popup
-        layer.bindPopup(feature.properties.City);
-        
-        
+        //layer.bindPopup(feature.properties.City);
         //function for every time a city is clicked - could replace the .on in markCities
         layer.on("click", function (e) {
             currentCity = feature.properties.Order;
